512cf254ef27828926bca8769e5b912c40d16f53
akdarid <matveytetevin0372@gmail.com>
2025-10-20 00:59:35 -0700

    start_4lab

--- 2zdn.py
+++
@@ -1,6 +0,0 @@
-a = (input("a: "))
-b = (input("b: "))
-while "," in a or "," in b:
-    a = a.replace(",", ".")
-    b = b.replace(",", ".")
-print(f'sum={float(a) + float(b)}; avg = {(float(a) + float(b)) / 2}')
