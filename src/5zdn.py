2e05e0ccd4dad94d5c7693b4bd5019507a724687
akdarid <matveytetevin0372@gmail.com>
2025-11-10 23:31:30 -0800

    full_5_lab_with ahuennoe readme

--- src/5zdn.py
+++
@@ -1,6 +0,0 @@
-fio = (input("ФИО: "))
-FIO = fio.split()
-print(f"Инициалы: {FIO[0][0]}{FIO[1][0]}{FIO[2][0]}.")
-print(f"Длина (символы) {len(fio)}")
-
-
--- src/zdn_1.py
+++ src/zdn_1.py
@@ -1,8 +1,3 @@
-# Лабораторная работа №5
-## Задание А - JSON ↔ CSV
-```python
-
-
 
 import csv
 import json
@@ -51,41 +46,4 @@ def csv_to_json(csv_path: str, json_path: str) -> None: #функция конв
                                                                 #indent=4 - красивое форматирование с отступами
 csv_to_json(r"/Users/matvejtetevin/laba/src/date/samples/people.csv", r"/Users/matvejtetevin/laba/src/date/out/people_from_csv.json")
 
-json_to_csv( r"/Users/matvejtetevin/laba/src/date/samples/people.json",  r"/Users/matvejtetevin/laba/src/date/out/people_from_json.csv" ) 
-```
-## Задание B - CSV → XLSX
-```python
-import os
-import csv
-import sys
-
-from openpyxl import Workbook #из библиотеки openpyx1 импортирует только класс Workbook для создаия Excel файлов
-
-def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None: #функция конвертанции CSV в XLSX
-    if not os.path.exists(csv_path): #если не существует файл по указанному пути, то
-        raise FileNotFoundError #выводит сообщение об ошибке
-        
-
-    if os.path.getsize(csv_path) == 0: #получает размер в байтах и проверяет не равен ли он 0 (пустой файл)
-        raise ValueError
-        
-    wb = Workbook() #создаем новую Excel книгу
-    ws = wb.active #берем первый лист
-    ws.title = "Sheet1" #переменовываем лист в Sheet1
-
-    with open(csv_path, "r", encoding="utf-8") as csv_file: #безопасно открывае файл для прочтения(автомвтически закрывает после использовния #csv_path - путь к файлу
-        reader = csv.reader(csv_file) #создает объект для чтения CSV файла
-        for row in reader: #перебирает каждую строку в CSV файле, row - переменная, содержащая данные одной строки(как список)
-            ws.append(row) #добавляет строку данных в Excel лист
-
-#Настройка ширины колонок
-    for column_cells in ws.columns: #перебирает содержания ячейки одной колонки и возвращает все колонки листа
-        max_length = 0 #создает переменную для хранения максимальной длины теста в колонке
-        column_letter = column_cells[0].column_letter #присваивает букву колонки
-        for cell in column_cells: #перебирает все ячейки в текущей колонке
-            if cell.value: #проверяет есть ли значение в ячейки(не пустая)
-                max_length = max(max_length, len(str(cell.value))) #обновляет длину строкиб сравнивая прошлое значение с настоящим
-        ws.column_dimensions[column_letter].width = max(max_length + 2, 8) #обращается к настройкам ширины конкретной колонки, устанавливает ширину колонки, максимальная длина + 2 символа для отступа, 8- выбирает большее значение между расчитанной шириной и минимальной шириной 8
-    wb.save(xlsx_path) #сохраняет Excel книгу по указанному пути
-csv_to_xlsx(r"/Users/matvejtetevin/laba/src/date/samples/cities.csv", r"/Users/matvejtetevin/laba/src/date/out/people.xlsx")
-```  
+json_to_csv( r"/Users/matvejtetevin/laba/src/date/samples/people.json",  r"/Users/matvejtetevin/laba/src/date/out/people_from_json.csv" )
